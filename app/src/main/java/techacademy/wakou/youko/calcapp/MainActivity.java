17b0e87a3ba07855005f4db37c1e15528eedf7d4
youko-wakou <appu2pudding@gmail.com>
2017-12-02 23:16:00 +0900

    SecondActivity値受け渡し

--- app/src/main/java/techacademy/wakou/youko/calcapp/MainActivity.java
+++ app/src/main/java/techacademy/wakou/youko/calcapp/MainActivity.java
@@ -9,7 +9,9 @@ import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+//    変数宣言
+    EditText editText1;
+    EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +29,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(this);
 
-        EditText editText1= (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText)findViewById(R.id.editText2);
+//        テキストエディタiD取得
+        editText1= (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText)findViewById(R.id.editText2);
     }
     @Override
     public void onClick(View v){
+        String val1 = editText1.toString();
+        String val2 = editText2.toString();
         Intent intent = new Intent(this,SecondActivity.class);
+//        テキストエディタ遷移先への値
+        intent.putExtra("VALUE1",val1);
+        intent.putExtra("VALUE2",val2);
+
+        if(v.getId() == R.id.button1) {
+            intent.putExtra("VALUE3","1");
+
+         }else if(v.getId() == R.id.button2){
+            intent.putExtra("VALUE3","2");
+        }else if(v.getId() == R.id.button3){
+             intent.putExtra("VALUE3","3");
+        }else if(v.getId() == R.id.button4){
+            intent.putExtra("VALUE3","4");
+        }
         startActivity(intent);
+
     }
 }
--- app/src/main/java/techacademy/wakou/youko/calcapp/SecondActivity.java
+++ app/src/main/java/techacademy/wakou/youko/calcapp/SecondActivity.java
@@ -2,6 +2,10 @@ package techacademy.wakou.youko.calcapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int value1 = intent.getIntExtra("VALUE1",0);
+        int value2 = intent.getIntExtra("VALUE2",0);
+        int value3 = intent.getIntExtra("VALUE3",0);
+
+            TextView textView = (TextView) findViewById(R.id.textView);
+
+            if (value3 == 1) {
+                textView.setText(String.valueOf(value1 - value2));
+            } else if (value3 == 2) {
+                textView.setText(String.valueOf(value1 + value2));
+            } else if (value3 == 3) {
+                textView.setText(String.valueOf(value1 * value2));
+            } else if (value3 == 4) {
+                textView.setText(String.valueOf(value1 / value2));
+            }
+
     }
 }
