a7a866b5339686b80aef5fc1e8d2362ba9c4f307
youko-wakou <appu2pudding@gmail.com>
2017-12-02 20:22:42 +0900

    Initial commit

---
+++ app/src/main/java/techacademy/wakou/youko/calcapp/MainActivity.java
@@ -0,0 +1,38 @@
+package techacademy.wakou.youko.calcapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        EditText editText1= (EditText) findViewById(R.id.editText1);
+        EditText editText2 = (EditText)findViewById(R.id.editText2);
+    }
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+}
